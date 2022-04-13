1b2032535c27c01fbb202c59e09961ccb4311a36
MarioFLS <mariofernandolsantos@gmail.com>
2022-04-13 08:09:34 -0300

    feat(Login): Criação de página de Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Login() {
-  return (<h2>Oi</h2>);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const verifyUser = () => {
+
+  };
+  const handleClickLogin = (event) => {
+    event.preventDefault();
+    history.push('/home');
+  };
+  return (
+    <main>
+      <form>
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Digite seu Nome"
+          onChange={({ target }) => setName(target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Digite seu Email"
+          onChange={({ target }) => setEmail(target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button type="submit" onClick={handleClickLogin}>Login</button>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
