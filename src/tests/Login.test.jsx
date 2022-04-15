c5e63a4248517692be9ccef30149270e77cad8f9
MarioFLS <mariofernandolsantos@gmail.com>
2022-04-15 15:04:20 -0300

    teste: BOtão de Login Redireciona para página de Home

--- src/tests/Login.test.jsx
+++ src/tests/Login.test.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { screen } from '@testing-library/react';
-/* import userEvent from '@testing-library/user-event'; */
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -20,5 +20,32 @@ describe('Teste Página de Login', () => {
     expect(inputName).toBeInTheDocument();
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
+  });
+
+  it('Se o botão de Login Redireciona para página Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const name = 'Usuário';
+    const email = 'email@teste.com';
+    const password = '123456';
+
+    const inputName = screen.getByPlaceholderText('Digite seu Nome');
+    const inputEmail = screen.getByPlaceholderText('Digite seu Email');
+    const inputPassword = screen.getByPlaceholderText('Digite sua senha');
+    const buttonLogin = screen.getByRole('button', { name: /Login/ });
+
+    expect(buttonLogin).toBeInTheDocument();
+    expect(buttonLogin).toBeDisabled();
+
+    userEvent.type(inputName, name);
+    userEvent.type(inputEmail, email);
+    userEvent.type(inputPassword, password);
+
+    expect(buttonLogin).not.toBeDisabled();
+
+    userEvent.click(buttonLogin);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/home');
   });
 });
