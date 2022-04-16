cfe316a9000afc9684cc2eea353bb1971aa80e98
MarioFLS <mariofernandolsantos@gmail.com>
2022-04-15 21:14:59 -0300

    feat:Criação de galeria das imagens

---
+++ src/gallery.js
@@ -0,0 +1,29 @@
+const gallery = [
+  {
+    name: 'Imagem Fantasy',
+    image: 'https://t3.ftcdn.net/jpg/02/81/42/82/360_F_281428216_YWRTOqeBWBmtuWxBci02ClnEnI22Fh7e.jpg',
+    type: 'fantasy',
+  },
+  {
+    name: 'Hobbit',
+    image: 'https://f.i.uol.com.br/fotografia/2012/11/21/211950-400x600-1.jpeg',
+    type: 'geek',
+  },
+  {
+    name: 'Ctulhu',
+    image: 'https://i0.wp.com/orbedosdragoes.com/wp-content/uploads/2020/12/O_Chamado_de_Cthulhu_Regras-1.jpg?resize=760%2C428&ssl=1',
+    type: 'terror',
+  },
+  {
+    name: 'Realista',
+    image: 'https://img.elo7.com.br/product/original/36723E1/desenho-realista-auto-retrato-a4-e-a3-colorido-a4.jpg',
+    type: 'realist',
+  },
+  {
+    name: 'Bob Esponja',
+    image: 'https://img.elo7.com.br/product/zoom/319792B/painel-de-festa-bob-esponja-festa-infantil.jpg',
+    type: 'cartoon',
+  },
+];
+
+export default gallery;
