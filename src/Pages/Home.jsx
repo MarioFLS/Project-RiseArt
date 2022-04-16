2e5d0d46c158f3a41beeaf2aadb5375449c4db86
MarioFLS <mariofernandolsantos@gmail.com>
2022-04-15 21:30:09 -0300

    feat: map da galeria na página home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ArtContext from '../context/ArtContext';
 
 function Home() {
-  return (<h2>a</h2>);
+  const { gallery } = useContext(ArtContext);
+  return (
+    <main>
+      <h1>Home</h1>
+      <div>
+        {gallery.map(({ name, image, type }) => (
+          <div key={name}>
+            <p>{name}</p>
+            <img width="200px" src={image} alt="Imagem" />
+            <p>{type}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default Home;
--- src/context/ArtProvider.jsx
+++ src/context/ArtProvider.jsx
@@ -1,9 +1,10 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ArtContext from './ArtContext';
+import gallery from '../gallery';
 
 function ArtProvider({ children }) {
-  const context = useMemo(() => ({}), []);
+  const context = useMemo(() => ({ gallery }), []);
 
   return (
     <ArtContext.Provider value={context}>
