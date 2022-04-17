fb2bc0621c87b890d2bbabcf93826a6798b63e6c
MarioFLS <mariofernandolsantos@gmail.com>
2022-04-17 17:23:09 -0300

    refactor: Alteração do map da Galeria, para trazer uma facilitação na hora de estilizar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ArtContext from '../context/ArtContext';
+import '../css/Home.css';
 
 function Home() {
   const { galleryObj } = useContext(ArtContext);
+  const [galleryonScreen, setGalleryonScreen] = useState([]);
+
+  const itemArr = (index) => Math.floor(index / 3);
+
+  useEffect(() => {
+    const galleryArr = [];
+    galleryObj.forEach((galleryItens, index) => {
+      if (index % 3 === 0) galleryArr.push([]);
+      galleryArr[itemArr(index)] = [...galleryArr[itemArr(index)], galleryItens];
+    });
+
+    setGalleryonScreen(galleryArr);
+  }, [setGalleryonScreen]);
+
   return (
-    <main>
+    <main className="main-container">
       <h1>Home</h1>
-      <div>
-        {galleryObj.map(({ name, image }) => (
-          <div key={name}>
-            <p>{name}</p>
-            <img width="200px" src={image} alt="Imagem" />
+      <div className="cards-container">
+        {galleryonScreen.map((item, index) => (
+          <div key={item[itemArr(index)].id}>
+            {item
+              .map(({ id, image }) => (
+                <div key={id}>
+                  <img src={image} alt="a" />
+                </div>
+              ))}
           </div>
         ))}
       </div>
--- src/gallery.js
+++ src/gallery.js
@@ -1,29 +1,48 @@
 const gallery = [
   {
+    id: 0,
     name: 'Imagem Fantasy',
     image: 'https://t3.ftcdn.net/jpg/02/81/42/82/360_F_281428216_YWRTOqeBWBmtuWxBci02ClnEnI22Fh7e.jpg',
     type: 'fantasy',
     curtidas: 0,
   },
   {
+    id: 1,
     name: 'Hobbit',
     image: 'https://f.i.uol.com.br/fotografia/2012/11/21/211950-400x600-1.jpeg',
     type: 'geek',
     curtidas: 0,
   },
   {
+    id: 2,
     name: 'Ctulhu',
     image: 'https://i0.wp.com/orbedosdragoes.com/wp-content/uploads/2020/12/O_Chamado_de_Cthulhu_Regras-1.jpg?resize=760%2C428&ssl=1',
     type: 'terror',
     curtidas: 0,
   },
   {
+    id: 3,
     name: 'Realista',
     image: 'https://img.elo7.com.br/product/original/36723E1/desenho-realista-auto-retrato-a4-e-a3-colorido-a4.jpg',
     type: 'realist',
     curtidas: 0,
   },
   {
+    id: 4,
+    name: 'Bob Esponja',
+    image: 'https://img.elo7.com.br/product/zoom/319792B/painel-de-festa-bob-esponja-festa-infantil.jpg',
+    type: 'cartoon',
+    curtidas: 0,
+  },
+  {
+    id: 5,
+    name: 'Bob Esponja',
+    image: 'https://img.elo7.com.br/product/zoom/319792B/painel-de-festa-bob-esponja-festa-infantil.jpg',
+    type: 'cartoon',
+    curtidas: 0,
+  },
+  {
+    id: 6,
     name: 'Bob Esponja',
     image: 'https://img.elo7.com.br/product/zoom/319792B/painel-de-festa-bob-esponja-festa-infantil.jpg',
     type: 'cartoon',
